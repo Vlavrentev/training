6e129cc920a32452e72c679b8a4f6e22cf66c5a2
Vlavrentev <37065222+Vlavrentev@users.noreply.github.com>
2018-06-18 12:57:02 +0300

    add test files

---
+++ cycles/ocenky.php
@@ -0,0 +1,15 @@
+<?php 
+$rates = array(3, 5, 3, 2, 1, 8, 4, 3, 4, 3, 2, 3);
+
+/* Посчитаем среднюю оценку. Для этого найдем сумму всех оценок, и поделим 
+ее на их количество */
+function funck($rates) {
+
+$ratesSum = array_sum($rates);
+$ratesCount = count($rates);; /* Количество оценок — эту строчку надо дописать самому */
+$averageRate = $ratesSum / $ratesCount ; /* Средняя оценка — допиши сам */
+$str = "Анону поставили {$ratesCount} оценок, их сумма = {$ratesSum}\n" . 'Средний балл — ' . round($averageRate, 2);
+return $str;
+}
+echo funck($rates);
+?>
---
+++ cycles/otvet.php
@@ -0,0 +1,14 @@
+<?php 
+$answers=['Возможно', 'Но это не точно', 'Да нет наверное', 'Да', 'Нет', 'В любом случае'];
+$oracul = rand(1, 5);
+function funck ($answers, $oracul) {
+foreach ($answers as $num => $ans) {
+    if ($oracul==$num){
+        $str = 'Ответ номер '. ($oracul + 1) .' - '. $ans . PHP_EOL;
+        break;
+    }
+}
+return $str;
+}
+echo funck($answers, $oracul);
+?>
