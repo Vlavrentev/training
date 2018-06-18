cd14b8b13f326ed6c09aa7a3f3a4f0efe8475055
Vlavrentev <37065222+Vlavrentev@users.noreply.github.com>
2018-06-18 13:01:12 +0300

    Add files via upload

---
+++ arrays/generator_imen.php
@@ -0,0 +1,27 @@
+<?php
+
+$namepart= [
+        'ку',
+        'ги',
+        'рю',
+        'ка',
+        'ми',
+        'ру',
+        'су',
+        'ги',
+        'та',
+        'дзу',
+        'на',
+        'ро',
+        'со',
+        'за',
+        'чи'
+    ];
+    
+$wid=rand(1,8);
+function funck ($namepart, $wid){
+ for ($name=[], $i=0; $i <= $wid; $i++){
+    echo implode ($name=[$namepart[rand(0,14)]]);
+ }
+}
+echo funck ($namepart, $wid);
