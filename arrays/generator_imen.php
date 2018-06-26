35c1ad964d8785872d299e84f796f6fdcbe895b8
Vlavrentev <37065222+Vlavrentev@users.noreply.github.com>
2018-06-26 18:58:34 +0300

    Update generator_imen.php

--- arrays/generator_imen.php
+++ arrays/generator_imen.php
@@ -1,32 +1,34 @@
 <?php
 
-$namepart= [
-        'га',
-        'му',
-        'ку',
-        'ги',
-        'рю',
-        'ка',
-        'ми',
-        'ру',
-        'су',
-        'ги',
-        'та',
-        'дзу',
-        'на',
-        'ро',
-        'со',
-        'за',
-        'чи'
-    ];
-    
-$wid=rand(1,8);
-function nameGenerator ($namepart, $wid) {
- for ($name=[], $i=0; $i <= $wid; $i++){
- $str = implode ([implode ($name=[$namepart[rand(0,16)]])]);
- $string[]=$str;
- }
- return implode ($string);
+$namepart = [
+    'га',
+    'му',
+    'ку',
+    'ги',
+    'рю',
+    'ка',
+    'ми',
+    'ру',
+    'су',
+    'ги',
+    'та',
+    'дзу',
+    'на',
+    'ро',
+    'со',
+    'за',
+    'чи'
+];
+
+$wid = rand(1, 8);
+function nameGenerator($namepart, $wid)
+{
+    for ($name = [], $i = 0; $i <= $wid; $i++) {
+        $str = implode([implode($name = [$namepart[rand(0, 16)]])]);
+        $string[] = $str;
+    }
+    return implode($string);
 }
+
 $string = nameGenerator($namepart, $wid);
-echo $string ;
+echo $string;
