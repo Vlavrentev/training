c9a7bc41f7b27496e3fbc51e22113ac4e79dbaa5
Vlavrentev <37065222+Vlavrentev@users.noreply.github.com>
2018-06-26 19:02:51 +0300

    Update rost.php

--- cycles/rost.php
+++ cycles/rost.php
@@ -10,33 +10,34 @@ $anonHeight = 169; /* Рост анона */
 
 /* Рост одноклассников анона */
 $classmates = array(
-  'Антон'  =>  172,
-  'Семен'  =>  165,
-  'Лена'  =>  189,
-  'Иван'  =>  171,
-  'Петр'  =>  182,
-  'Сидор'  =>  176,
-  'Аня'    =>  180,
-  'Таня'  =>  179,
-  'Маня'  =>  171
+    'Антон' => 172,
+    'Семен' => 165,
+    'Лена' => 189,
+    'Иван' => 171,
+    'Петр' => 182,
+    'Сидор' => 176,
+    'Аня' => 180,
+    'Таня' => 179,
+    'Маня' => 171
 );
-function classHeight ($anonHeight, $classmates) {
-/* Перебираем всех одноклассников */
-foreach ($classmates as $name => $height) {
-     $string[]="Имя: {$name}, рост: {$height} см. ";
-    if ($height>$anonHeight) {
-        $overh[$name]=$height;
-    $string[]= 'при этом выше анона ' . PHP_EOL;
-    } 
-    else {
-    $string[]= 'при этом ниже анона ' . PHP_EOL;}
-    /* Тут надо добавить проверку, выше или ниже этот человек, чем анон,
-    и подсчитать число тех, кто выше */
+function classHeight($anonHeight, $classmates)
+{
+    /* Перебираем всех одноклассников */
+    foreach ($classmates as $name => $height) {
+        $string[] = "Имя: {$name}, рост: {$height} см. ";
+        if ($height > $anonHeight) {
+            $overh[$name] = $height;
+            $string[] = 'при этом выше анона ' . PHP_EOL;
+        } else {
+            $string[] = 'при этом ниже анона ' . PHP_EOL;
+        }
+        /* Тут надо добавить проверку, выше или ниже этот человек, чем анон,
+        и подсчитать число тех, кто выше */
+    }
+
+    $number = count($overh);
+    $number = "В классе {$number} человек выше анона\n";
+    return implode($string) . $number;
 }
 
-$number = count($overh);
-$number = "В классе {$number} человек выше анона\n";
-return implode ($string). $number;
-}
-echo classHeight ($anonHeight, $classmates);
-
+echo classHeight($anonHeight, $classmates);
