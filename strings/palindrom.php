55c39ed668cf51e41de8673425f91f29acfebb05
Vlavrentev <37065222+Vlavrentev@users.noreply.github.com>
2018-06-26 19:03:11 +0300

    Update palindrom.php

--- strings/palindrom.php
+++ strings/palindrom.php
@@ -1,16 +1,17 @@
 <?php
 mb_internal_encoding('utf-8');
 echo $str = "А роза упала на лапу Азора";
-function palindrome ($str) {
-$str = mb_strtolower ($str = str_replace(" ", "", $str)) . PHP_EOL;
-$letters1 = preg_split('//u', $str, null, PREG_SPLIT_NO_EMPTY);
-unset($letters1[count($letters1) - 1]);
-$letters2 = array_values(array_reverse($letters1, true));
-if ($letters1 === $letters2)
-    {$answ = " это - Палиндром";}
-    else $answ = " это - не палиндром";
-return $answ;    
+function palindrome($str)
+{
+    $str = mb_strtolower($str = str_replace(" ", "", $str)) . PHP_EOL;
+    $letters1 = preg_split('//u', $str, null, PREG_SPLIT_NO_EMPTY);
+    unset($letters1[count($letters1) - 1]);
+    $letters2 = array_values(array_reverse($letters1, true));
+    if ($letters1 === $letters2) {
+        $answ = " это - Палиндром";
+    } else $answ = " это - не палиндром";
+    return $answ;
 }
 
-echo palindrome ($str);
+echo palindrome($str);
 ?>
