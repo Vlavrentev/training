71f5eb305a5cecea02f31aa61dc2d38e3a427e03
Vlavrentev <37065222+Vlavrentev@users.noreply.github.com>
2018-06-26 18:43:19 +0300

    Update ocenky.php

--- cycles/ocenky.php
+++ cycles/ocenky.php
@@ -1,9 +1,9 @@
 <?php 
-$rates = array(3, 5, 3, 2, 1, 8, 4, 3, 4, 3, 2, 3);
+$rates =[3, 5, 3, 2, 1, 8, 4, 3, 4, 3, 2, 3];
 
 /* Посчитаем среднюю оценку. Для этого найдем сумму всех оценок, и поделим 
 ее на их количество */
-function funck($rates) {
+function testResults($rates) {
 
 $ratesSum = array_sum($rates);
 $ratesCount = count($rates);; /* Количество оценок — эту строчку надо дописать самому */
@@ -11,5 +11,5 @@ $averageRate = $ratesSum / $ratesCount ; /* Средняя оценка — до
 $str = "Анону поставили {$ratesCount} оценок, их сумма = {$ratesSum}\n" . 'Средний балл — ' . round($averageRate, 2);
 return $str;
 }
-echo funck($rates);
+echo testResults($rates);
 ?>
