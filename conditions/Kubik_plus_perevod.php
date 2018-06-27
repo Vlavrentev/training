4fb06f58a9a1d9479bdcac2659071678331e6578
Vlavrentev <37065222+Vlavrentev@users.noreply.github.com>
2018-06-27 16:50:49 +0300

    Update Kubik_plus_perevod.php

--- conditions/Kubik_plus_perevod.php
+++ conditions/Kubik_plus_perevod.php
@@ -10,11 +10,11 @@ function coubAndcourse($rub, $usd, $hum, $comp)
         'Случайное число на кубике - ' . rand(1, 6) . PHP_EOL;
 
     if ($hum > $comp) {
-        $winner = ('Человек выйграл комп со счетом ' . $hum . ' - ' . $comp);
+        $winner = 'Человек выйграл комп со счетом ' . $hum . ' - ' . $comp;
     } elseif ($hum < $comp) {
-        $winner = ('Человек проиграл компу со счетом ' . $hum . ' - ' . $comp);
+        $winner = 'Человек проиграл компу со счетом ' . $hum . ' - ' . $comp;
     } else {
-        $winner = ("Ничья: $hum - $comp");
+        $winner = "Ничья: $hum - $comp";
     }
     return $cours . PHP_EOL . $winner;
 }
