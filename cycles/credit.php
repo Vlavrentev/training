7f1a47d085d0f204c9eb936934b9f27912780c7d
Vlavrentev <37065222+Vlavrentev@users.noreply.github.com>
2018-06-27 16:33:09 +0300

    Update credit.php

--- cycles/credit.php
+++ cycles/credit.php
@@ -1,30 +1,74 @@
 <?php
-//Для замены банка необходмо передать в функцию другой массив
-$homoc = [1.04, 500, 0, "Homocredit"];
-$soft = [1.03, 1000, 0, "Softbank"];
-$straw = [1.02, 0, 7777, "StrawberryBank"];
 
-
-function credit($percent, $servicePayment, $FixPayment, $bank)
+class Credit
 {
-    for ($month = 1, $paymentTotal = 0, $monthlyPayment = 5000, $creditBalance = 40000; $month <= 20; $month++) {
+    private $percent;
+    private $servicePayment;
+    private $FixPayment;
+    private $bank;
 
-        $creditBalance = ($creditBalance * $percent) + $servicePayment - $monthlyPayment;
-        $paymentTotal = $paymentTotal + $monthlyPayment;
+    public function __construct($percent, $servicePayment, $FixPayment, $bank)
+    {
+        $this->percent = $percent;
+        $this->servicePayment = $servicePayment;
+        $this->FixPayment = $FixPayment;
+        $this->bank = $bank;
 
-        if ($creditBalance < 0) {
-            if ($FixPayment != 0) {
-                $str = "С меня хватит!\n" . "За " . $month . " месяцев выплачено банку " . $bank . ", всего " . $vsego = round($paymentTotal + $creditBalance, 2) + $FixPayment . " руб. \n";
-                $str = $str . "Переплатил - " . $pereplata = $vsego - 40000 . " руб.";
+    }
+
+
+    public function getPercent()
+    {
+        return $this->percent;
+    }
+
+
+    public function getServicePayment()
+    {
+        return $this->servicePayment;
+    }
+
+
+    public function getFixPayment()
+    {
+        return $this->FixPayment;
+    }
+
+    public function getBank()
+    {
+        return $this->bank;
+    }
+
+    public static function creditCalculate($bankInf)
+    {
+        /**
+         * @param Credit $bankInf
+         */
+        for ($month = 1, $paymentTotal = 0, $monthlyPayment = 5000, $creditBalance = 40000; $month <= 20; $month++) {
+
+            $creditBalance = ($creditBalance * $bankInf->getPercent()) + $bankInf->getServicePayment() - $monthlyPayment;
+            $paymentTotal = $paymentTotal + $monthlyPayment;
+
+            if ($creditBalance < 0) {
+                if ($bankInf->getFixPayment() != 0) {
+                    $str = "С меня хватит!\n" . "За " . $month . " месяцев выплачено банку " . $bankInf->getBank() . ", всего " . $only = round($paymentTotal + $creditBalance, 2) + $bankInf->getFixPayment() . " руб. \n";
+                    $str = $str . "Переплатил - " . $pereplata = $only - 40000 . " руб.";
+                    return $str;
+                    break;
+                } else
+                    $str = "С меня хватит!\n" . "За " . $month . " месяцев выплачено банку " . $bankInf->getBank() . ", всего " . $only = round($paymentTotal + $creditBalance, 2) . " руб. \n";
+                $str = $str . "Переплатил - " . $pereplata = $only - 40000 . " руб.";
                 return $str;
                 break;
-            } else
-                $str = "С меня хватит!\n" . "За " . $month . " месяцев выплачено банку " . $bank . ", всего " . $vsego = round($paymentTotal + $creditBalance, 2) . " руб. \n";
-            $str = $str . "Переплатил - " . $pereplata = $vsego - 40000 . " руб.";
-            return $str;
-            break;
+            }
         }
     }
 }
 
-echo $itog = credit($percent = $straw[0], $servicePayment = $straw[1], $FixPayment = $straw[2], $bank = $straw[3]);
+$bankInf1 = new Credit (1.04, 500, 0, "Homocredit");
+$bankInf2 = new Credit (1.03, 1000, 0, "Softbank");
+$bankInf3 = new Credit (1.02, 0, 7777, "StrawberryBank");
+
+echo $results = Credit::creditCalculate($bankInf1) . PHP_EOL;
+echo $results = Credit::creditCalculate($bankInf2) . PHP_EOL;
+echo $results = Credit::creditCalculate($bankInf3) . PHP_EOL;
